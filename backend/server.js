3b210f0f0f73e43ab90973a2fcf9fada5cd67830
Tanisha Maurya <12tanishamaurya@gmail.com>
2024-04-23 19:38:34 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 //npm run server
 
-
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
@@ -14,12 +14,20 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 
 import {app,server} from "./socket/socket.js"
 
+const __dirname=path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth',authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(res,req)=>{
+ res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
 
 server.listen(PORT,()=>{
  connectToMongoDB();
